5b7aee42a4d657387bf5ba7939427993a8de7731
Sahil Satishkumar <sahilsatishkumar@gmail.com>
2019-12-04 09:45:51 -0800

    removed trailing text

--- src/components/TripInfoModal.js
+++ src/components/TripInfoModal.js
@@ -117,7 +117,6 @@ const TripInfoModal = ({
                       </tr>
                     ))}
                   </tbody>
-                  {JSON.stringify(errors)}
                 </HTMLTable>
               </div>
             ) : null}
