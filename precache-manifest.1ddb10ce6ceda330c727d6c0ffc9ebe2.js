4a2846400d830a4994c6bfd7f9284f5696be6f00
Sahil Satishkumar <sahilsatishkumar@gmail.com>
2019-12-04 16:38:36 -0800

    Updates

---
+++ precache-manifest.1ddb10ce6ceda330c727d6c0ffc9ebe2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5061515138e0ef0225d5a372192425",
+    "url": "/atlas/index.html"
+  },
+  {
+    "revision": "a60602a392d0328746ee",
+    "url": "/atlas/static/css/2.f5ee52bb.chunk.css"
+  },
+  {
+    "revision": "f184e456ab7b66b0aa75",
+    "url": "/atlas/static/css/main.4b45d2a5.chunk.css"
+  },
+  {
+    "revision": "a60602a392d0328746ee",
+    "url": "/atlas/static/js/2.f686ecb3.chunk.js"
+  },
+  {
+    "revision": "f184e456ab7b66b0aa75",
+    "url": "/atlas/static/js/main.3390eb23.chunk.js"
+  },
+  {
+    "revision": "057db67afcf533279067",
+    "url": "/atlas/static/js/runtime-main.7b94fc83.js"
+  },
+  {
+    "revision": "782154b83b6b419269980b375144b1f7",
+    "url": "/atlas/static/media/icons-16.782154b8.ttf"
+  },
+  {
+    "revision": "7fb844a70d4ff3aa49441242b4149777",
+    "url": "/atlas/static/media/icons-16.7fb844a7.woff"
+  },
+  {
+    "revision": "d4f6722b1ac527dd937155d419ec8ddd",
+    "url": "/atlas/static/media/icons-16.d4f6722b.eot"
+  },
+  {
+    "revision": "90afdd237a469c44c9a1a3b0ff2db197",
+    "url": "/atlas/static/media/icons-20.90afdd23.eot"
+  },
+  {
+    "revision": "c0e4874e23b8c9100e566ca2ca287128",
+    "url": "/atlas/static/media/icons-20.c0e4874e.woff"
+  },
+  {
+    "revision": "d1f0f6c1a1c239a03914f7bd06ccfe68",
+    "url": "/atlas/static/media/icons-20.d1f0f6c1.ttf"
+  }
+]);
