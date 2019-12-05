8851e78f9917964dea213b0513d8afd0658a4fe6
Sahil Satishkumar <sahilsatishkumar@gmail.com>
2019-12-05 14:48:47 -0800

    gist support added

--- src/App.js
+++ src/App.js
@@ -19,11 +19,14 @@ const params = new URLSearchParams(window.location.search);
 
 const loadDefault = params.get("loadDefault") === "true";
 
+const gistURL = params.get("gist");
+
 function App() {
   const [showTripModal, updateShowTripModal] = React.useState(true);
   const [json, updateJson] = React.useState(
     loadDefault ? defaultJSON : undefined
   );
+  const [error, updateError] = React.useState(false);
   const mapRef = hooks.useMap(mapContainerId);
   const popupRef = hooks.usePopup(mapRef);
 
@@ -39,7 +42,20 @@ function App() {
   };
 
   React.useEffect(() => {
-    if (loadDefault) window.history.replaceState({}, "", "?loadDefault=false");
+    if (gistURL) {
+      const gistId = gistURL.split("/").pop();
+      fetch(`https://api.github.com/gists/${gistId}`)
+        .then(response => response.json())
+        .then(data => {
+          if (data.message) updateError(data.message);
+          else {
+            const json = JSON.parse(data.files["default.json"].content);
+            updateJson(json);
+          }
+        })
+        .catch(error => updateError(error));
+    } else if (loadDefault)
+      window.history.replaceState({}, "", "?loadDefault=false");
     else {
       const previousJSON = localStorage.getItem("previousJSON");
       const tripJSON = JSON.parse(previousJSON);
@@ -55,6 +71,7 @@ function App() {
     <div className="app-container">
       <div id={mapContainerId} />
       <TripInfoModal
+        fetchError={error}
         trip={json}
         showTripModal={showTripModal}
         showModal={() => updateShowTripModal(true)}
